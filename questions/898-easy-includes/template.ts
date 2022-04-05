97ce90ced762a372263bf21d83edb89151be20db
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2022-04-06 00:02:39 +0200

    solution 898-easy-includes

--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,1 +1,10 @@
-type Includes<T extends readonly any[], U> = any;
+type Equal<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
+
+export type Includes<T extends readonly any[], U> =
+  T extends [infer HEAD, ...infer TAIL]
+    ? Equal<HEAD, U> extends true
+      ? true
+      : Includes<TAIL, U>
+    : false
