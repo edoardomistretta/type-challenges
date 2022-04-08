ce55176a3a4f890d5e97703d450bc1c483d2ba9d
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2022-04-08 13:58:26 +0200

    solution 15-medium-last

--- questions/15-medium-last/template.ts
+++ questions/15-medium-last/template.ts
@@ -1,1 +1,6 @@
-type Last<T extends any[]> = any
+export type Last<I> =
+  I extends [infer H1, ...infer T1]
+    ? T1 extends []
+      ? H1
+      : Last<T1>
+    : never
--- questions/15-medium-last/test-cases.ts
+++ questions/15-medium-last/test-cases.ts
@@ -1,4 +1,5 @@
 import { Equal, Expect } from '@type-challenges/utils'
+import { Last } from './template'
 
 type cases = [
   Expect<Equal<Last<[3, 2, 1]>, 1>>,
