12d4ff3e4311915241fcd603919a931eb07b8760
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2022-04-11 22:42:15 +0200

    solution 20-medium-promise-all

--- questions/20-medium-promise-all/template.ts
+++ questions/20-medium-promise-all/template.ts
@@ -1,1 +1,5 @@
-declare function PromiseAll(values: any): any
+export declare function PromiseAll<T extends unknown[]>(values: readonly [...T]): Promise<
+  {
+    [key in keyof T]: T[key] extends Promise<infer A> ? A : T[key]
+  }
+>
