4efd0daa83e63ad077d23d952119614102c7f82a
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2022-04-12 15:23:46 +0200

    solution 116-medium-replace

--- questions/116-medium-replace/template.ts
+++ questions/116-medium-replace/template.ts
@@ -1,1 +1,6 @@
-type Replace<S extends string, From extends string, To extends string> = any
+export type Replace<S extends string, From extends string, To extends string> =
+  From extends ''
+    ? S
+    : `${S}` extends `${infer H}${From}${infer T}`
+      ? `${H}${To}${T}`
+      : S
--- questions/116-medium-replace/test-cases.ts
+++ questions/116-medium-replace/test-cases.ts
@@ -1,4 +1,5 @@
 import { Equal, Expect } from '@type-challenges/utils'
+import { Replace } from './template'
 
 type cases = [
   Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
