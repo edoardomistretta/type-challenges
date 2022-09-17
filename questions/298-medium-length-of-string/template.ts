84f59a445566ebb5b5356b9224bd7005f998f6bf
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2022-09-17 18:01:20 +0200

    solution 298-medium-length-of-string

--- questions/298-medium-length-of-string/template.ts
+++ questions/298-medium-length-of-string/template.ts
@@ -1,1 +1,8 @@
-type LengthOfString<S extends string> = any
+type StringToArray<S extends string> =
+  S extends `${infer H}${infer T}`
+    ? T extends ""
+      ? [H]
+      : [H, ...StringToArray<T>]
+    : []
+
+export type LengthOfString<S extends string> = StringToArray<S>["length"]
